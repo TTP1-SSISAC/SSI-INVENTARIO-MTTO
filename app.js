1bafe38eb9a00fb5a70532b702044aedd2933977
TRANSPORTES SSI <richardferando2005@gmail.com>
2026-09-12 03:41:58 -0500

    Update API_URL in app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ Consume el Google Sheet a través del Web App de Apps Script.
 
 const CONFIG = {
 // Pega aquí la URL de tu implementación de Apps Script (termina en /exec)
-API_URL: 'https://script.google.com/macros/s/AKfycbzudGUOUgbGmTIKxansrEv9OGf3z2TWfD_xhS1M0YCxR4-sXjKPbt--F6QLjbiLybV4/exec'
+API_URL: 'https://script.google.com/macros/s/AKfycbwcSuaUvR_k4ZVywU9DIApee4Jurx97dQBKMekljtcOOyDa0eqIB1nkqUH5KQOAM88Q/exec'
 };
 
 const state = {
